e3f4a2a0282871a31022a2c9578116b333248460
WAshbourne22 <108470473+WAshbourne22@users.noreply.github.com>
2022-08-22 19:36:51 +0100

    correct starter code

--- Project.py
+++ Project.py
@@ -19,8 +19,9 @@ def run():
     stat_choice = input('Which stat do you want to use? (id, height, weight) ')
     opponent_pokemon = random_pokemon()
     print('The opponent chose {}'.format(opponent_pokemon['name']))
-    my_stat = my_pokemon[stat_choice]
-    opponent_stat = opponent_pokemon[stat_choice]
+
+my_stat = my_pokemon[stat_choice]
+opponent_stat = opponent_pokemon[stat_choice]
 
 if my_stat > opponent_stat:
     print('You Win!')
