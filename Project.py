98a9a1a92dd25d9982e9e7c545dd8657d71536e8
maudmillar <111740604+maudmillar@users.noreply.github.com>
2022-08-23 20:49:55 +0200

    just added a time lag for effect

--- Project.py
+++ Project.py
@@ -13,13 +13,11 @@ def random_pokemon():
 'weight': pokemon['weight'],
 }
 
-# hello world
-
 def run():
     my_pokemon = random_pokemon()
     print('You were given {}'.format(my_pokemon['name']))
-    stat_choice = input('Which stat do you want to use? (id, height, weight) ')
-    opponent_pokemon = random_pokemon()
+stat_choice = input('Which stat do you want to use? (id, height, weight) ')
+opponent_pokemon = random_pokemon()
     print('The opponent chose {}'.format(opponent_pokemon['name']))
 
 my_stat = my_pokemon[stat_choice]
@@ -33,3 +31,42 @@ else:
     print('Draw!')
 
 run()
+#
+# print("Welcome to Pokemon Top Trumps!")
+# print("Player 1, your pokemon is...")
+#
+# #1. Generate a random number between 1 and 151 to use as the Pokemon ID number:
+# pokemon_number = (random.randint(1,151))
+#
+# #2. Using the Pokemon API get a Pokemon based on its ID number:
+# url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
+#
+# #3. Create a dictionary that contains the returned pokemon's name, id, height and weight:
+# response = requests.get(url)
+# pokemon = response.json()
+#
+# print(pokemon["name"])
+# print("ID Number:")
+# print(pokemon["id"])
+# print("Height:")
+# print(pokemon["height"])
+# print("Weight:")
+# print(pokemon["weight"])
+#
+# #4. Get another random pokemon for their opponent:
+# print("Player 2, your pokemon is...")
+# pokemon_number = (random.randint(1,151))
+# url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
+# response = requests.get(url)
+# pokemon = response.json()
+#
+# print(pokemon["name"])
+# print("ID Number:")
+# print(pokemon["id"])
+# print("Height:")
+# print(pokemon["height"])
+# print("Weight:")
+# print(pokemon["weight"])
+#
+# #5. Ask the user which stat they want to use (id, height or weight)
+# input("Which stat do you want to use?")
