7f436392202fb19d2ee630380eb04c85177c2ee5
maudmillar <111740604+maudmillar@users.noreply.github.com>
2022-08-25 12:58:51 +0200

    Made a few changes to combine both sets of code. Everything is clearly titled with comments.
    1. Started by clearly organising both imports and definitions from the starter code
    
    2. Made the code imitate the real flow of Top Trumps more effectively. ie:
    - At the beginning of the game, welcome the player and display their "card" to them first (show them their pokemon's name, id, height and weight)
    - Ask them to choose a stat before seeing the opponent's stats
    - Then display the opponent's stats, once the user has chosen which stat is in play
    
    3. To-do: Allow the user to input a stat to compare, and get the programme to return a "win, lose or draw" result.

--- Project.py
+++ Project.py
@@ -1,72 +1,77 @@
+# Imports
 import random
 import requests
+import time
 
+# Definitions
 def random_pokemon():
     pokemon_number = random.randint(1, 151)
     url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
     response = requests.get(url)
     pokemon = response.json()
     return {
-'name': pokemon['name'],
-'id': pokemon['id'],
-'height': pokemon['height'],
-'weight': pokemon['weight'],
-}
+    'name': pokemon['name'],
+    'id': pokemon['id'],
+    'height': pokemon['height'],
+    'weight': pokemon['weight']
+    }
 
-def run():
-    my_pokemon = random_pokemon()
-    print('You were given {}'.format(my_pokemon['name']))
-stat_choice = input('Which stat do you want to use? (id, height, weight) ')
-opponent_pokemon = random_pokemon()
-    print('The opponent chose {}'.format(opponent_pokemon['name']))
+# Game beginning
+print("Welcome to Pokemon Top Trumps!")
+print("Your pokemon is...")
 
-my_stat = my_pokemon[stat_choice]
-opponent_stat = opponent_pokemon[stat_choice]
+# Generate a random number between 1 and 151 to use as the Pokemon ID number:
+pokemon_number = (random.randint(1,151))
 
-if my_stat > opponent_stat:
-    print('You Win!')
-elif my_stat < opponent_stat:
-    print('You Lose!')
-else:
-    print('Draw!')
+# Using the Pokemon API get a Pokemon based on its ID number:
+url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
 
-run()
+# Create a dictionary that contains the returned pokemon's name, id, height and weight:
+response = requests.get(url)
+pokemon = response.json()
 
-# print("Welcome to Pokemon Top Trumps!")
-# print("Player 1, your pokemon is...")
+# Display the pokemon and its stats to the player.
+print(pokemon["name"])
+time.sleep(2)
+print("ID Number:")
+print(pokemon["id"])
+print("Height:")
+print(pokemon["height"])
+print("Weight:")
+print(pokemon["weight"])
+
+time.sleep(2)
+
+# Ask the user which stat they want to use (id, height or weight)
+print("It's time to play.")
+print("Pick a stat (ID, Height, Weight).")
+print("If it's higher than your opponent's, you win!")
+print("If it's lower, you lose.")
+print("If it's the same, you draw.")
+time.sleep(2)
+input("Which stat do you want to use?")
+
+# Allow the user to decide on a stat for comparison (to do)
 #
-# #1. Generate a random number between 1 and 151 to use as the Pokemon ID number:
-# pokemon_number = (random.randint(1,151))
 #
-# #2. Using the Pokemon API get a Pokemon based on its ID number:
-# url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
 #
-# #3. Create a dictionary that contains the returned pokemon's name, id, height and weight:
-# response = requests.get(url)
-# pokemon = response.json()
+
+# Get another random pokemon for their opponent and display its stats:
+print("The opponent's pokemon is...")
+pokemon_number = (random.randint(1,151))
+url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
+response = requests.get(url)
+pokemon = response.json()
+print(pokemon["name"])
+time.sleep(2)
+print("ID Number:")
+print(pokemon["id"])
+print("Height:")
+print(pokemon["height"])
+print("Weight:")
+print(pokemon["weight"])
+
+# Return a win, lose or draw result (to do)
 #
-# print(pokemon["name"])
-# print("ID Number:")
-# print(pokemon["id"])
-# print("Height:")
-# print(pokemon["height"])
-# print("Weight:")
-# print(pokemon["weight"])
 #
-# #4. Get another random pokemon for their opponent:
-# print("Player 2, your pokemon is...")
-# pokemon_number = (random.randint(1,151))
-# url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
-# response = requests.get(url)
-# pokemon = response.json()
 #
-# print(pokemon["name"])
-# print("ID Number:")
-# print(pokemon["id"])
-# print("Height:")
-# print(pokemon["height"])
-# print("Weight:")
-# print(pokemon["weight"])
-#
-# #5. Ask the user which stat they want to use (id, height or weight)
-# input("Which stat do you want to use?")
