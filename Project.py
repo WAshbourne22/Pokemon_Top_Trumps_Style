d60676885b741deee149277b4e03d71b65abfb7f
maudmillar <111740604+maudmillar@users.noreply.github.com>
2022-08-26 15:01:13 +0200

    Added High Score programme

--- Project.py
+++ Project.py
@@ -60,3 +60,62 @@ elif my_stat < opponent_stat:
     print('No win this time')
 else:
     print('It looks like a Draw!')
+
+# High Score Programme
+
+def get_high_score():
+    # Default high score
+    high_score = 0
+
+# Try to read the high score from a file
+    try:
+        high_score_file = open("high_score.txt", "r")
+        high_score = int(high_score_file.read())
+        high_score_file.close()
+        print("The high score is", high_score)
+    except IOError:
+        # Error reading file, no high score
+        print("There is no high score yet.")
+    except ValueError:
+        # There's a file there, but it doesn't understand the number.
+        print("I'm confused. Starting with no high score.")
+
+    return high_score
+
+def save_high_score(new_high_score):
+    try:
+        # Write the file to disk
+        high_score_file = open("high_score.txt", "w")
+        high_score_file.write(str(new_high_score))
+        high_score_file.close()
+    except IOError:
+        # Can't write it.
+        print("Unable to save the high score.")
+
+
+def main():
+    """ Main programme is here. """
+    # Get the high score
+    high_score = get_high_score()
+
+    # Get the score from the current game
+    current_score = 0
+    try:
+        # Ask the user for their score
+        current_score = int(input("What is your score? "))
+    except ValueError:
+        # Error, they didn't type a number
+        print("I don't understand what you typed.")
+
+    # See if we have a new high score
+    if current_score > high_score:
+        # Yes: save to file
+        print("Yea! New high score!")
+        save_high_score(current_score)
+    else:
+        print("Better luck next time.")
+
+
+# Call the main function, start up the game
+if __name__ == "__main__":
+    main()
